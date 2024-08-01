43b8146f97f6ca82fee6e5e183182aafff785727
Mayank <myank07official@gmail.com>
2024-08-01 22:06:27 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
 axios.defaults.withCredentials = true;  // Include credentials with requests
 axios.defaults.baseURL = 'https://shorten-link-beta.vercel.app'// Optional: set base URL
