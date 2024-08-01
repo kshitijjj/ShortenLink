ea2d9a63a06d817da55491eb1c5252700ebfbc65
Mayank <myank07official@gmail.com>
2024-08-01 19:00:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ var shortUrl = require("node-url-shortener");
 
 const corsOptions = {
     origin: 'https://shorten-link-frontend-henna.vercel.app', // Your front-end domain
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials:true
   };
 
   app.use(cors(corsOptions));
